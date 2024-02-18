77cf95189eccf7f736b46951fb0af3c11155fb97
guopeng <381694179@qq.com>
2024-02-18 20:04:38 +0800

    add:17 18 19 20

---
+++ nowcoder/huawei/tips.js
@@ -0,0 +1,24 @@
+// # str
+
+let str = "123";
+// 1.字符串获取第一位，获取剩余几位
+str[0];
+str.substring(1, str.length);
+str.parseInt(2);
+str.parsefloot;
+let num = 123;
+num.parseFloat(2);
+parseFloat(string);
+// # arr
+let arr = ["a", "b", "c"];
+// 1. forEach是数组方法
+arr.forEach((index, item) => {
+  console.log(index, item, "index,item"); //a 0 index,item
+});
+
+// # 循环语句 for
+
+//1.continue break return 区别？
+// continue 只能在循环语句中使用 跳过当前循环 继续下一次判定
+// break  终止循环
+// return 跳出当前方法
